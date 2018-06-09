34510f61b7726467339fbca6af28bf7b3a39fe71
Leonardo Quintana Juarez <30363785+JorgeAntrax@users.noreply.github.com>
2018-06-09 14:12:50 -0600

    Change scroll height

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 document.querySelector('main').onscroll = function() {
     var scroll = this.scrollTop || document.body.scrollTop;
-    if (scroll > 100) {
+    if (scroll > 1000) {
         document.querySelector('#bottom-nav').classList.add('show');
     } else {
         document.querySelector('#bottom-nav').classList.remove('show');
