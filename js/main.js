b201c4da99f3df51efd00f2c44debd93cacce17e
jorgeantrax <spicer_94@hotmail.com>
2018-06-26 14:18:48 -0600

    Added navbar and updated layout design

--- js/main.js
+++ js/main.js
@@ -1,80 +1,135 @@
-document.querySelector('main').onscroll = function() {
-    var scroll = this.scrollTop || document.body.scrollTop;
-    if (scroll > 100) {
-        document.querySelector('#bottom-nav').classList.add('show');
-    } else {
-        document.querySelector('#bottom-nav').classList.remove('show');
-    }
-};
+let $navMain = document.querySelector('#nav-main');
+let $closeBtnMenu = document.querySelectorAll('.close-menu-btn, #show-docs');
+let $sidebar = document.querySelector('#menu-principal');
+let $snippet = document.querySelectorAll('snippet pre');
+let $menu = document.querySelectorAll('[data-link]');
+let $menuContent = document.querySelectorAll('.menu_content');
 
-var snippet = document.querySelectorAll('snippet pre')
+let $toggleTabsDemo = document.querySelectorAll('#toggle-size-tabs button');
+let $tab5 = document.querySelector('#tabs5');
 
-for (var i = 0; i < snippet.length; i++) {
-    snippet[i].addEventListener('click', function() {
-        this.style.maxHeight = '100%';
-        this.style.overflowY = 'hidden';
-    }, false);
-}
+let $toggleTabsA = document.querySelectorAll('#toggle-align-tabs button');
+let $tab6 = document.querySelector('#tabs6');
 
-var menu = document.querySelectorAll('#m1 menu-item:not(.is-label)');
-var menuContent = document.querySelectorAll('.menu_content');
+let $demoBDivider = document.querySelector('#demo-breadcrumb-toggle-divider');
+let $toggleButtonDivider = document.querySelectorAll('#demo-breadcrumb-divider > button');
+let $printDBread = document.getElementById('demo-breadcrumb-doc');
+let $demoABread = document.querySelector('#breadcrumb-Align-demo');
+let $toggleBread = document.querySelectorAll('#bread-align-buttons > button');
+let $spanABread = document.querySelector('#demo-align-bread');
+let $menuItems = document.querySelectorAll('menu-item[data-link]');
+let $searchComponent = document.querySelector('#search-component');
+let $searchInput = $searchComponent.querySelector('.input');
+let $searchList = $searchComponent.querySelector('#searchList');
+let $stickyBtn = document.querySelector('#demo-sticky-btn');
+/* Serach component list */
 
-for (var i = 1; i < menuContent.length; i++) {
-    menuContent[i].style.display = "none";
-}
-for (var i = 0; i < menu.length; i++) {
-    menu[i].addEventListener('click', function() {
-        var activeItem = this.getAttribute('data-link');
-        for (var i = 0; i < menuContent.length; i++) {
-            menu[i].classList.remove("is-active");
-            menuContent[i].style.display = "none";
+$stickyBtn.addEventListener('click', function() {
+    document.querySelector('#demo-sticky').remove();
+}, false);
+
+$searchInput.addEventListener('keyup', function(e) {
+    $searchList.innerHTML = '';
+    e.stopPropagation();
+    let search = this.value;
+    for (let i = 0; i < $menuItems.length; i++) {
+        let text = $menuItems[i].querySelector('span').outerText;
+        let cadena = ~text.toLowerCase().indexOf(search);
+        if (cadena != 0) {
+            let attr = $menuItems[i].getAttribute('data-link');
+            updateList(attr, text);
         }
-        this.classList.add("is-active");
-        document.getElementById(activeItem).style.display = "block";
-    }, false);
-}
-
-document.querySelector('#show-password').addEventListener('click', function() {
-    var input = this.previousElementSibling.getAttribute('type');
-    var e = this.previousElementSibling;
-    if (input == 'text') {
-        e.setAttribute('type', 'password');
-    } else if (input == 'password') {
-        e.setAttribute('type', 'text');
+    }
+    if (!$searchList.classList.contains('is-visible')) {
+        $searchList.classList.add('is-visible');
     }
 }, false);
 
-var sidebar = document.querySelector('#menu-principal');
-var sidebarClose = sidebar.querySelector('#menu-close');
 
-document.querySelector('#mostrar-side-menu').addEventListener('click', function() {
-    sidebar.style.left = 0;
-    sidebar.style.backgroundColor = 'rgba(0,0,0, .8)';
-    sidebarClose.style.left = 0;
-    sidebarClose.style.width = '100%';
+function updateList(attr, text) {
+    console.log($searchList);
+    $searchList.innerHTML += `<list-item data-link="${attr}"><span>${text}</span><icon><i class="fa fa-cube"></i></icon></list-item>`;
+    let item = $searchList.querySelectorAll('list-item');
+    for (let a = 0; a < item.length; a++) {
+        item[a].addEventListener('click', function() {
+            $searchInput.value = text;
+            let activeItem = this.getAttribute('data-link');
+            for (let i = 0; i < $menuContent.length; i++) {
+                if ($menuContent[i].classList.contains('is-visible')) {
+                    $menuContent[i].classList.remove('is-visible');
+                }
+                $menu[i].classList.remove("is-active");
+            }
+            document.getElementById(activeItem).classList.add('is-visible');
+            $searchList.classList.remove('is-visible');
+        }, false);
+    }
+}
 
-}, false);
+/* $sidebar visibility */
 
-sidebarClose.addEventListener('click', function() {
-    sidebar.style.left = '-150%';
-    this.style.left = '-150%';
-    this.style.width = 0;
-}, false);
-
-var toggleTabsDemo = document.querySelectorAll('#toggle-size-tabs button');
-var tab5 = document.querySelector('#tabs5');
-
-var toggleTabsA = document.querySelectorAll('#toggle-align-tabs button');
-var tab6 = document.querySelector('#tabs6');
-
-for (var i = 0; i < toggleTabsDemo.length; i++) {
-    toggleTabsDemo[i].addEventListener('click', function() {
-        demoTabs(tab5, 'is-small', 'is-medium', 'is-large', this.innerText);
+if ($navMain) {
+    window.addEventListener('scroll', function() {
+        if (window.scrollY > 400) {
+            $navMain.classList.add('is-fixed')
+        } else {
+            $navMain.classList.remove('is-fixed')
+        }
     }, false);
 }
-for (var j = 0; j < toggleTabsA.length; j++) {
-    toggleTabsA[j].addEventListener('click', function() {
-        demoTabs(tab6, 'is-right', 'is-center', 'is-fluid', this.innerText);
+
+$snippet.forEach(el => {
+    el.addEventListener('click', function() {
+        el.classList.add('is-expand');
+        console.log(el);
+    }, false);
+});
+
+$menuContent[0].classList.add('is-visible');
+$menu[0].classList.add('is-active');
+
+if ($menu.length > 0) {
+    for (let i = 0; i < $menu.length; i++) {
+        if ($menu[i].classList.contains('is-title') || $menu[i].classList.contains('ignore')) {} else {
+            $menu[i].addEventListener('click', function() {
+                let activeItem = this.getAttribute('data-link');
+                for (let i = 0; i < $menuContent.length; i++) {
+                    if ($menuContent[i].classList.contains('is-visible')) {
+                        $menuContent[i].classList.remove('is-visible');
+                    }
+                    $menu[i].classList.remove("is-active");
+                }
+                this.classList.add("is-active");
+                document.getElementById(activeItem).classList.add('is-visible');
+                $sidebar.classList.remove('is-visible');
+            }, false);
+        }
+    }
+}
+
+if ($closeBtnMenu) {
+    $closeBtnMenu.forEach(btn => {
+        if (btn.classList.contains('floating')) {
+            btn.classList.add('animation-onda');
+
+            setTimeout(function() {
+                btn.classList.remove('animation-onda');
+            }, 60000);
+        }
+        btn.addEventListener('click', function() {
+            $sidebar.classList.toggle('is-visible');
+        }, false);
+    });
+}
+
+for (let i = 0; i < $toggleTabsDemo.length; i++) {
+    $toggleTabsDemo[i].addEventListener('click', function() {
+        demoTabs($tab5, 'is-small', 'is-medium', 'is-large', this.innerText);
+    }, false);
+}
+for (let j = 0; j < $toggleTabsA.length; j++) {
+    $toggleTabsA[j].addEventListener('click', function() {
+        demoTabs($tab6, 'is-right', 'is-center', 'is-fluid', this.innerText);
     }, false);
 }
 
@@ -90,41 +145,33 @@ function demoTabs(el, cls1, cls2, cls3, element) {
         remove(el, cls1 + ' ' + cls3);
     } else if (element == cls3) {
         el.classList.add(cls3);
-        remove(tab6, cls2 + ' ' + cls1);
+        remove($tab6, cls2 + ' ' + cls1);
     }
 }
 
 
 function remove(el, array) {
     array = array.split(' ');
-    for (var i = 0; i < array.length; i++) {
+    for (let i = 0; i < array.length; i++) {
         el.classList.remove(array[i]);
     }
 }
 
-var demoBDivider = document.querySelector('#demo-breadcrumb-toggle-divider');
-var toggleButtonDivider = document.querySelectorAll('#demo-breadcrumb-divider > button');
-var printDBread = document.getElementById('demo-breadcrumb-doc');
-
-for (let i = 0; i < toggleButtonDivider.length; i++) {
-    toggleButtonDivider[i].addEventListener('click', function() {
-        toggleDemo(demoBDivider, printDBread, 'is-dotted', 'is-arrowed', this.innerText);
+for (let i = 0; i < $toggleButtonDivider.length; i++) {
+    $toggleButtonDivider[i].addEventListener('click', function() {
+        toggleDemo($demoBDivider, $printDBread, 'is-dotted', 'is-arrowed', this.innerText);
     }, false);
 }
 
-var demoABread = document.querySelector('#breadcrumb-Align-demo');
-var toggleBread = document.querySelectorAll('#bread-align-buttons > button');
-var spanABread = document.querySelector('#demo-align-bread');
-
-for (let i = 0; i < toggleBread.length; i++) {
-    toggleBread[i].addEventListener('click', function() {
-        toggleDemo(demoABread, spanABread, 'is-center', 'is-right', this.innerText);
+for (let i = 0; i < $toggleBread.length; i++) {
+    $toggleBread[i].addEventListener('click', function() {
+        toggleDemo($demoABread, $spanABread, 'is-center', 'is-right', this.innerText);
     }, false);
 }
 
-var SBread = document.querySelector('#size-bread');
-var TSBread = document.querySelectorAll('#toggle-size-bread > button');
-var SSBread = document.querySelector('#demo-size-bread');
+let SBread = document.querySelector('#size-bread');
+let TSBread = document.querySelectorAll('#toggle-size-bread > button');
+let SSBread = document.querySelector('#demo-size-bread');
 
 for (let i = 0; i < TSBread.length; i++) {
     TSBread[i].addEventListener('click', function() {
@@ -149,7 +196,7 @@ function toggleDemo(el, tag, cls1, cls2, element) {
     }
 }
 (function(d, s, id) {
-    var js, fjs = d.getElementsByTagName(s)[0];
+    let js, fjs = d.getElementsByTagName(s)[0];
     if (d.getElementById(id)) return;
     js = d.createElement(s);
     js.id = id;
@@ -157,69 +204,62 @@ function toggleDemo(el, tag, cls1, cls2, element) {
     fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
-var webhost = document.querySelector('body > div');
-if (webhost != undefined) {
-    for (var i = 0; i < webhost.length; i++) {
-        webhost[i].remove();
-    }
-}
-
-var demoSelect = new Listbox({
+let demoSelect = new Listbox({
     el: '#demoSelect1',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var demo0 = new Listbox({
+let demo0 = new Listbox({
     el: '#demo0',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
-var demo1 = new Listbox({
+let demo1 = new Listbox({
     el: '#demo1',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var demo2 = new Listbox({
+let demo2 = new Listbox({
     el: '#demo2',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var demo3 = new Listbox({
+let demo3 = new Listbox({
     el: '#demo3',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var demo4 = new Listbox({
+let demo4 = new Listbox({
     el: '#demo4',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var demoIcons = new Listbox({
+let demoIcons = new Listbox({
     el: '#demoIcons',
     hasTwoIcons: true,
     iconToggleClass: 'fa fa-angle-down',
     iconLabelClass: 'fa fa-user-circle'
 });
 
-var demoA = new Listbox({
+let demoA = new Listbox({
     el: '#demoA',
     hasTwoIcons: true,
     iconToggleClass: 'fa fa-angle-down',
     iconLabelClass: 'fa fa-users'
 });
 
-var demoAtributes = new Listbox({
+let demoAtributes = new Listbox({
     el: '#demoAtributes',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
 });
 
-var cal2 = new Calendar({
+let cal2 = new Calendar({
     el: '#calendar2', // the Id container with class calendar-picker
     languaje: 'es',
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
@@ -228,7 +268,7 @@ var cal2 = new Calendar({
     iconPosition: 'left' // icon toggle position
 });
 
-var cald1 = new Calendar({
+let cald1 = new Calendar({
     el: '#CalendarDemo1', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -237,7 +277,7 @@ var cald1 = new Calendar({
     style: 'dark'
 });
 
-var cald2 = new Calendar({
+let cald2 = new Calendar({
     el: '#CalendarDemo2', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -245,7 +285,7 @@ var cald2 = new Calendar({
     iconPosition: 'left', // icon toggle position
     style: 'info'
 });
-var cald3 = new Calendar({
+let cald3 = new Calendar({
     el: '#CalendarDemo3', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -253,7 +293,7 @@ var cald3 = new Calendar({
     iconPosition: 'left', // icon toggle position
     style: 'success'
 });
-var cald4 = new Calendar({
+let cald4 = new Calendar({
     el: '#CalendarDemo4', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -261,7 +301,7 @@ var cald4 = new Calendar({
     iconPosition: 'left', // icon toggle position
     style: 'warning'
 });
-var cald5 = new Calendar({
+let cald5 = new Calendar({
     el: '#CalendarDemo5', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -270,7 +310,7 @@ var cald5 = new Calendar({
     style: 'danger'
 });
 
-var cald6 = new Calendar({
+let cald6 = new Calendar({
     el: '#CalendarDemo6', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-caret-up', // the classes for the icon previus control
     classIconNext: 'fa fa-caret-down', // the classess for the icon next control
@@ -278,7 +318,7 @@ var cald6 = new Calendar({
     iconPosition: 'left' // icon toggle position
 });
 
-var cald7 = new Calendar({
+let cald7 = new Calendar({
     el: '#CalendarDemo7', // the Id container with class calendar-picker
     classIconPrev: 'fa fa-angle-up', // the classes for the icon previus control
     classIconNext: 'fa fa-angle-down', // the classess for the icon next control
@@ -286,9 +326,7 @@ var cald7 = new Calendar({
     iconPosition: 'right' // icon toggle position
 });
 
-
-
-var demoF = new Listbox({
+let demoF = new Listbox({
     el: '#demoField',
     hasTwoIcons: false,
     iconToggleClass: 'fa fa-angle-down'
